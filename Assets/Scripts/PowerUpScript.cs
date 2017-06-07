ef69d905cd25710ad0d85dc77424c6cb5bba703b
Suril Shah <suril.shah.94@gmail.com>
2017-06-06 23:28:59 -0400

    first commit

---
+++ Assets/Scripts/PowerUpScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpScript : MonoBehaviour {
+
+	HUDScript hud;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			hud = GameObject.Find ("Main Camera").GetComponent<HUDScript> ();
+			hud.IncreaseScore (10);
+			Destroy (this.gameObject);
+		}
+	}
+}
